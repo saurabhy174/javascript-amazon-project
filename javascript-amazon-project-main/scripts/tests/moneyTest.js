8e2de88216d508e7e5896f558a79a7b1c5d37af9
saurabhy174 <saurabhyvr1@gmail.com>
2025-11-04 17:48:34 +0530

    Created automated tests for formatCurrency

---
+++ javascript-amazon-project-main/scripts/tests/moneyTest.js
@@ -0,0 +1,35 @@
+import { formatCurrency } from "../utils/money.js";
+
+   console.log('testSuite = frormatCurrency');
+ console.log('convert cents into dollar');
+
+if(formatCurrency(2095) === '20.95'){     // basic test case 1 - test if code is working
+    console.log('passed');
+}
+else{
+    console.log('failed');
+}
+
+ console.log('works with 0');
+
+if(formatCurrency(0) === '0.00'){    //edge - test case 2- test with values that are tricky
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+ console.log('round up to the narest set');
+
+if(formatCurrency(2000.5)=== '20.01'){    //edge case 3
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+if(formatCurrency(2000.4)=== '20.00'){    //edge case 4
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+//group of test cases=testSuite
