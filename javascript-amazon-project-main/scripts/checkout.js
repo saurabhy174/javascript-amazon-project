43563450bd71392cf372e247f90b4fd64e9ca167
saurabhy174 <saurabhyvr1@gmail.com>
2025-11-09 12:33:29 +0530

    Created oops version of Cart

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,6 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/cart-oop.js';   // only run all code without impoerting anything
+
 renderOrderSummary();
 renderPaymentSummary();
